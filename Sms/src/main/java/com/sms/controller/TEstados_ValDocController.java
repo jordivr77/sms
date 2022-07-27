ba1a96f4e621c9b71351ee5ed5f94998b70df7d3
jordivr77 <jordivr77@gmail.com>
2022-07-27 13:38:06 +0200

    configuración CORS

--- Sms/src/main/java/com/sms/controller/TEstados_ValDocController.java
+++ Sms/src/main/java/com/sms/controller/TEstados_ValDocController.java
@@ -6,14 +6,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sms.service.TEstados_ValDocService;
 import com.sms.service.DTO.SelectedItemDTO;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET})
 @RequestMapping("/api")
 public class TEstados_ValDocController {
 	
